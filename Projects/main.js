b7e51352f473eb4eb7f81904f40328de350b03e7
Salieri009 <Jungwook.Van-1@student.uts.edu.au>
2025-04-19 16:44:57 +1000

    doing mergE

--- Projects/main.js
+++ Projects/main.js
@@ -1,6 +1,9 @@
 import {setScene, setSceneElements, controlCamera} from "/Projects/sceneManager.js";
 //import {setScene, setSceneElements, controlCamera} from "./sceneManager.js";
+import UIManager from "./UIManager";
+
 setScene();
 setSceneElements();
 controlCamera();
 
+const uiManager = new UIManager();
