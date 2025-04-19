08ec06b67d254f6a22ac6ae110181eca62a14946
Salieri009 <Jungwook.Van-1@student.uts.edu.au>
2025-04-19 15:42:31 +1000

    work on UI manager

--- Projects/UIManager.js
+++ Projects/UIManager.js
@@ -1,70 +1,20 @@
-gi/*
-import { GUI } from 'three/addons/libs/lil-gui.module.min.js';
-
+// UIManager.js
 export default class UIManager {
-    constructor(grassManager, animalManager) {
-        this.grassManager = grassManager;
-        this.animalManager = animalManager;
-
-        // GUI 초기화
-        this.gui = new GUI();
-
-        // 풀 설정 섹션 추가
-        this._setupGrassControls();
-
-        // 동물 설정 섹션 추가
-        this._setupAnimalControls();
+    constructor() {
+        this.createUI();
     }
 
-    _setupGrassControls() {
-        const folder = this.gui.addFolder('Grass Settings');
-
-        // 풀 개수 조정
-        folder.add(this.grassManager.params, 'count', 100, 5000)
-            .name('Count')
-            .step(100)
-            .onChange(() => this.grassManager.createGrass());
-
-        // 풀 높이 조정
-        folder.add(this.grassManager.params, 'height', 0.1, 3.0)
-            .name('Height');
-
-        // 풀 성장 속도 조정
-        folder.add(this.grassManager.params, 'growthSpeed', 0.1, 2.0)
-            .name('Growth Speed');
-
-        // 풀 색상 변경
-        folder.addColor(this.grassManager.params, 'color')
-            .name('Color')
-            .onChange(color => this.grassManager.updateGrassColor(color));
-
-        folder.open();
+    createUI() {
+        const div = document.createElement("div");
+        div.innerHTML = "<h1 style='position: absolute; top: 10px; left: 10px; color: white;'>UI Manager</h1>";
+        document.body.appendChild(div);
     }
 
-    _setupAnimalControls() {
-        const folder = this.gui.addFolder('Animal Settings');
+    updateScore(score) {
+        // 점수 갱신하는 로직
+    }
 
-        // 동물 개수 조정
-        folder.add(this.animalManager.params, 'count', 1, 20)
-            .name('Count')
-            .step(1)
-            .onChange(() => this.animalManager.createAnimals());
-
-        // 동물 크기 조정
-        folder.add(this.animalManager.params, 'size', 0.1, 2.0)
-            .name('Size')
-            .onChange(() => this.animalManager.createAnimals());
-
-        // 동물 이동 속도 조정
-        folder.add(this.animalManager.params, 'speed', 0.01, 0.1)
-            .name('Speed');
-
-        // 동물 색상 변경
-        folder.addColor(this.animalManager.params, 'color')
-            .name('Color')
-            .onChange(color => this.animalManager.updateAnimalColor(color));
-
-        folder.open();
+    showMessage(msg) {
+        // 메시지를 화면에 띄우는 로직
     }
 }
-*/
--- Projects/main.js
+++ Projects/main.js
@@ -1,5 +1,6 @@
 import {setScene, setSceneElements, controlCamera} from "/Projects/sceneManager.js";
-//import {setScene, setSceneElements, controlCamera} from "./sceneManager.js";
+
+
 setScene();
 setSceneElements();
 controlCamera();
