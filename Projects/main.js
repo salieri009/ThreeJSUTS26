5f67c1e87ffed38c17afaeef015bdfb9172c56d5
Salieri009 <Jungwook.Van-1@student.uts.edu.au>
2025-04-19 18:34:09 +1000

    changing

--- Projects/main.js
+++ Projects/main.js
@@ -1,6 +1,10 @@
-import {setScene, setSceneElements, controlCamera} from "/Projects/sceneManager.js";
+import { setScene, setSceneElements, controlCamera } from "/Projects/sceneManager.js";
+import UIManager from "./UIManager.js";
 
+// Initialize the Three.js scene and controls
 setScene();
 setSceneElements();
 controlCamera();
 
+// Create an instance of UIManager
+const ui = new UIManager();
