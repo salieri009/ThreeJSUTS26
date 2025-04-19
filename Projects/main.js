2115702df3c9b55c780adecd93b7faafffd5591e
Salieri009 <Jungwook.Van-1@student.uts.edu.au>
2025-04-19 16:12:28 +1000

    few changes

--- Projects/main.js
+++ Projects/main.js
@@ -1,7 +1,8 @@
-import {setScene, setSceneElements, controlCamera} from "/Projects/sceneManager.js";
-
+import { setScene, setSceneElements, controlCamera } from "/Projects/sceneManager.js";
+import UIManager from './UIManager.js'; // UIManager import
 
 setScene();
 setSceneElements();
 controlCamera();
 
+const uiManager = new UIManager(); // UIManager 인스턴스 생성
