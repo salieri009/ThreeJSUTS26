b983ede754adef0427d0837947f5845f4fada676
Salieri009 <Jungwook.Van-1@student.uts.edu.au>
2025-05-30 19:34:15 +1000

    Fixng

--- Projects/main.js
+++ Projects/main.js
@@ -1,38 +1,26 @@
 import { setScene, controlCamera} from "./scripts/sceneManager.js";
 import { animate, loadScene } from "./scripts/gridModels.js";
-
-import {  cloudMove, loadClouds, setBackground, sun, createRain, updateRain, removeRain,
-    createSnow, updateSnow, removeSnow,
-    createStorm, updateStorm, removeStorm,
-    setWeather} from "./scripts/environment.js";
-
+import {  cloudMove, loadClouds, setBackground, sun, } from "./scripts/environment.js";
 import { init } from "./scripts/UIManager.js";
 import { addBlock, deleteModel } from "./scripts/buttonInteract.js";
 // import * as seasonSyncUtil from './seasonSyncUtil';
 
-//==================================================//
+//Scene & Camera Setup
+// - Initialize the Three.js scene, camera, and renderer
 setScene();
 controlCamera();
 loadScene();
+//Begin rendering
 animate();
-//===================Weather========================//
 setBackground();
 sun();
 loadClouds();
 cloudMove();
 
-// 날씨 버튼 이벤트 연결//
-//Test Function //
-
-
-// weatherAnimate();
-
-
-//=====================================================//
+// Terrain & Object Controls
+// - Enable terrain expansion and model deletion
 addBlock();
 deleteModel();
-//=============UI Calling=====================================//
-// initSeasonSyncUtil();
 
-// initClock();
+
 init();
